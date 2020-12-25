0c84c03e2201dd654db2e666d3b3df60a885fe60
langlanglang <jinren.lang@gmail.com>
2020-12-25 16:28:25 +0800

    init TODO

---
+++ dist/static/Done.2b2e36ac.js
@@ -0,0 +1,1 @@
+import{d as a,j as e,t,T as s,a as o,F as m}from"./index.03267997.js";import{T as d}from"./TodoItem.0e96be03.js";var i=a((()=>()=>e(m,null,t.todos.filter((a=>a.status===s.FINISHED)).map((a=>e(d,{key:a.id,name:a.name,btnText:"Delete",onBtnClick:()=>o(a.id)}))))));export default i;
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { BuildConfig } from 'vite'
+
+const config: BuildConfig = {
+  base: '',
+  assetsDir: 'static',
+  // entry: '',
+  // outDir: '',
+  // assetsInlineLimit: {},
+  // cssCodeSplit: {}
+}
+
+export default config
