9fdf56a3e8ba44669076bf383ceb501e8cedea9a
langlanglang <jinren.lang@gmail.com>
2020-12-25 16:44:56 +0800

    update docs

---
+++ docs/static/Done.fc66c02c.js
@@ -0,0 +1,1 @@
+import{d as a,j as t,t as e,T as s,a as d,F as o}from"./index.cdfd30f2.js";import{T as m}from"./TodoItem.7c45ad1d.js";var i=a((()=>()=>t(o,null,e.todos.filter((a=>a.status===s.FINISHED)).map((a=>t(m,{key:a.id,name:a.name,btnText:"Delete",onBtnClick:()=>d(a.id)}))))));export default i;
